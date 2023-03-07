3f0b0f8d0e32a2b76537e1f737445dc098854484
DanielAvshalumov <avshalumov.daniel@gmail.com>
2023-03-07 14:35:02 -0500

    boilerplate physical state

--- src/components/Physical/Physical.jsx
+++ src/components/Physical/Physical.jsx
@@ -1,25 +1,56 @@
-import { Divider, Grid, Paper, Typography, Box } from "@mui/material";
-import React, { useEffect } from "react"
+import { Divider, Grid, Typography, Box } from "@mui/material";
+import React, {  } from "react"
+
+
+function reducer(physical, action) {
+    switch (action.type) {
+        case(""): {
+
+        }
+        case(9): {
+            
+        }
+    }
+}
 
 const Physical = () => {
 
+    const state = {
+        calories : 0,
+        protein: 0,
+        carbs: 0,
+        fat: 0,
+        meals: [
+            {
+                mealName: "",
+                calories: 0,
+                protein: 0,
+                carbs: 0,
+                fat: 0
+            }  
+        ]
+    }
+    const [physical, dispatch] = useReducer(reducer, state);
 
     return (
         <React.Fragment>
             
             <Grid container>
-                <Grid item md={5} >
+                <Grid item md={5}>
                     <Typography variant="h3" textAlign="center">Macros</Typography>
-
-                    <Box>
-                        Current
+                    <Box display={"flex"} mt={3}>
+                        <Box ml={15}>
+                            Current
+                        </Box>
+                        <Box ml={15}>
+                            Goal
+                        </Box>
+                        <Box ml={15}>
+                            Left
+                        </Box>
                     </Box>
-                    <Box>
-                        Current
-                    </Box>
-                    
                 </Grid>
-                <Divider orientation="vertical" flexItem  />
+                <Divider orientation="vertical" flexItem />
                 <Grid item md={6}>
                     <Typography variant="h3" textAlign="center">Meals</Typography>
                 </Grid>
